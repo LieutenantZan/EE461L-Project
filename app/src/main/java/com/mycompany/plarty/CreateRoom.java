eaefbd9a9bb39fd65c17ba11a78778ce096a94d9
Zane Lewin <zanelewin@gmail.com>
2016-03-20 17:17:26 -0500

    Added Create Room activity

---
+++ app/src/main/java/com/mycompany/plarty/CreateRoom.java
@@ -0,0 +1,15 @@
+package com.mycompany.plarty;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class CreateRoom extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+
+    }
+}
